0b9b7f971a1be35ae467297b67e8dce551bf424b
Sandy <sandy@.(none)>
2012-03-20 15:38:59 -0400

    Cleaned up some stuff to make everything compile for the last commit

--- thesis/src/main/java/pls/SaRunner.java
+++
@@ -1,8 +0,0 @@
-package pls;
-
-public interface SaRunner<T extends SaSolution> {
-	/**
-	 * Returns the final state and the best state.
-	 */
-	public SaSolution[] run(T start, long timeMs);
-}
--- thesis/src/test/java/pls/TestChooserReducer.java
+++ thesis/src/test/java/pls/TestChooserReducer.java
@@ -8,22 +8,24 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import pls.vrp.VrpReducer;
+
 public class TestChooserReducer {
 	@Test
 	public void testChooseKBest() {
 		final int k = 3;
-		ChooserReducer.SolutionData[] solDatas = {
-				new ChooserReducer.SolutionData(5, null, -1, -1, -1, -1),
-				new ChooserReducer.SolutionData(10, null, -1, -1, -1, -1),
-				new ChooserReducer.SolutionData(4, null, -1, -1, -1, -1),
-				new ChooserReducer.SolutionData(3, null, -1, -1, -1, -1),
-				new ChooserReducer.SolutionData(27, null, -1, -1, -1, -1),
-				new ChooserReducer.SolutionData(1, null, -1, -1, -1, -1),
-				new ChooserReducer.SolutionData(4, null, -1, -1, -1, -1)
+		SolutionData[] solDatas = {
+				new SaSolutionData(5, null, -1, -1, -1, -1),
+				new SaSolutionData(10, null, -1, -1, -1, -1),
+				new SaSolutionData(4, null, -1, -1, -1, -1),
+				new SaSolutionData(3, null, -1, -1, -1, -1),
+				new SaSolutionData(27, null, -1, -1, -1, -1),
+				new SaSolutionData(1, null, -1, -1, -1, -1),
+				new SaSolutionData(4, null, -1, -1, -1, -1)
 		};
 		
-		ChooserReducer cr = new ChooserReducer();
-		List<ChooserReducer.SolutionData> kbest = cr.chooseKBest(new ArrayList<ChooserReducer.SolutionData>(Arrays.asList(solDatas)), k);
+		ChooserReducer cr = new VrpReducer();
+		List<SolutionData> kbest = cr.chooseKBest(new ArrayList<SolutionData>(Arrays.asList(solDatas)), k);
 		Assert.assertEquals(solDatas[2], kbest.get(0));
 		Assert.assertEquals(solDatas[3], kbest.get(1));
 		Assert.assertEquals(solDatas[5], kbest.get(2));
